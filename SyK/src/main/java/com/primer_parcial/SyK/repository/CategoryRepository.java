62ef9e06a43283d36b6350649730625173dc9152
wilderCepeda <134663720+wilderCepeda@users.noreply.github.com>
2023-10-01 17:26:08 -0500

    Delete SyK directory

--- SyK/src/main/java/com/primer_parcial/SyK/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.primer_parcial.SyK.repository;
-
-import com.primer_parcial.SyK.models.CategoryModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<CategoryModel, Long> {
-
-}
--- SyK/src/main/java/com/primer_parcial/SyK/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.primer_parcial.SyK.services;
-
-import com.primer_parcial.SyK.models.UserModel;
-import org.springframework.http.ResponseEntity;
-public interface UserService {
-    ResponseEntity<UserModel> createUser(UserModel user);
-
-    ResponseEntity<?> login(String email, String password);
-}
